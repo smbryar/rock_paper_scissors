7ca265a54bb99b3720db472a5e068851813e4ebc
Saoirse Bryar <smbryar@gmail.com>
2020-02-14 17:41:26 +0000

    Separated into smaller functions

--- index.js
+++ index.js
@@ -8,47 +8,81 @@ const readline = require("readline").createInterface({
     output: process.stdout
 });
 
-function handleUserResponse (response) {
-    // Validate user input
+function validateUserResponse (response) {
     const humanResponse = response.toLowerCase().replace(/[.\!\s]/g,"");
     if (humanResponse.match(/^(rock|paper|scissors)$/)) {
-        console.log('You have chosen '+humanResponse+'.');}
+        return humanResponse } 
     else {
-        console.log("I'm afraid that isn't one of the three options. The computer overlord wins by default.");
+        return undefined;} 
+}
+
+function compRandomChoice()  {
+    const random = Math.floor(Math.random()*3);
+    switch (random) {
+        case 0:
+            return 'rock';
+            break;
+        case 1:
+            return 'paper';
+            break;
+        case 2:
+            return 'scissors';
+            break;
+        default:
+            return undefined;
+        }
+    }
+
+function compareChoices(human,computer) {
+    if (human === computer) {
+        return undefined; }
+    else if ((human === 'rock' && computer === 'scissors') || 
+            (human === 'paper' && computer === 'rock') ||
+            (human === 'scissors' && computer === 'paper')) {
+        return true; }
+    else {
+        return false;}
+    }
+    
+
+function handleUserResponse (response) {
+    // Validate user input
+    const humanResponse = validateUserResponse (response);
+    
+    if (humanResponse === undefined) {
+        console.log("I'm afraid that isn't one of the three options. The computer overlord wins by default.")
+    }
+    else {
+        console.log("You have chosen "+humanResponse+".");
     }
     
     // Random choice for computer
-    const compChoice = Math.floor(Math.random()*3);
-    switch (compChoice) {
-        case 0:
-            console.log('The computer has chosen rock.');
-            break;
-        case 1:
-            console.log('The computer has chosen paper.');
-            break;
-        case 2:
-            console.log('The computer has chosen scissors.');
-            break;
-        default:
-            console.log('Uh oh, the computer has messed up. You win by default!');
+
+    const compChoice = compRandomChoice();
+
+    if (compChoice === undefined) {
+        console.log('Uh oh, the computer has messed up. You win by default!')
     }
+    else {
+        console.log('The computer has chosen '+compChoice+'.');
+    }
+    
 
     // Compare choices and report on who has won
-        if ((humanResponse === 'rock' && compChoice === 1) || 
-            (humanResponse === 'paper' && compChoice === 2) ||
-            (humanResponse === 'scissors' && compChoice === 0)) {
-            console.log('You win!');
-            }
-        else if ((humanResponse === 'rock' && compChoice === 0) || 
-                (humanResponse === 'paper' && compChoice === 1) ||
-                (humanResponse === 'scissors' && compChoice === 2)) {
-            console.group("It's a draw. How civilised.");}
-        else {
-            console.log('The computer got you this time!');
+
+    const result = compareChoices(humanResponse,compChoice);
+
+    if (result === true) {
+        console.log('You win!');
         }
+    else if (result === false) {
+        console.group('The computer got you this time!');}
+    else {
+        console.log("It's a draw. How civilised.");
+    }
     
     readline.close();
 }
 
 // Obtain choice from user
-readline.question('Which do you choose: rock, paper or scissors?\n', handleUserResponse); // Everything that happens with the user input has to happen within this function. new line character using \n
+readline.question('Which do you choose: rock, paper or scissors?\n', handleUserResponse); 
